3e91cf2e9d456f1eade8a1e1fa9dee853271cc4c
Nishes Joshi <nishesj@gmail.com>
2017-11-12 13:59:23 +0100

    Initial Commit

---
+++ ATB/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ATB
+//
+//  Created by Nishes Joshi on 12/11/2017.
+//  Copyright © 2017 Nishes Joshi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ATB/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ATB
+//
+//  Created by Nishes Joshi on 12/11/2017.
+//  Copyright © 2017 Nishes Joshi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
